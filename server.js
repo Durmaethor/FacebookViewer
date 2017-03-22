2e1be46c3c8ed885029251c7afe65c89c8e58a0d
Ryan Kramer <rkramer613@gmail.com>
2017-03-21 19:55:31 -0600

    Step 1

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+var app = express();
+
+passport.use(new FacebookStrategy({
+  clientID: '1506083429423908',
+  clientSecret: '1deb7fd14110848ce2aff9334eef165a',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function(token, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+app.listen(3000, function(){
+    console.log("We have ears");
+});
